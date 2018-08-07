907868f17d3e1a023230e856c35411980afa0dbb
Kai Pak <kai@kaipak.org>
2018-08-06 22:14:54 -0700

    Got to creating pared down test and train datasets.

---
+++ run_analysis.R
@@ -0,0 +1,19 @@
+library(tidyr)
+
+# Load 'X' training and test datasets and apply labels from features.txt. Cull
+# this down to only columns containing mean or stdev labels.
+features <- read.csv('UCI_HAR_ds/features.txt', header = FALSE, 
+                     sep=' ', col.names = c('num', 'desc'), check.names=FALSE)
+# Get rid of special characters that may cause parsing issues
+features$desc <- gsub("(-|\\(|\\)|\\,)", "", features$desc)
+wanted_feat <- features[grepl('(mean|std)', features$desc),]
+
+# Create the pared down test set
+X_test <- read.csv('UCI_HAR_ds/test/X_test.txt', header=FALSE, sep='')
+X_test <- select(X_test, wanted_feat$num)
+colnames(X_test) <- wanted_feat$desc
+
+# Create the pared down train set
+X_train <- read.csv('UCI_HAR_ds/train/X_train.txt', header=FALSE, sep='')
+X_train <- select(X_train, wanted_feat$num)
+colnames(X_train) <- wanted_feat$desc
